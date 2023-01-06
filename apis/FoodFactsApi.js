def6ffb86897e6f1caf287729e214011566d5c74
max mcruvie <mcruvie@hotmail.co.uk>
2023-01-06 21:55:08 +0000

    Barcode error handle, product list, data cleaning

--- apis/FoodFactsApi.js
+++ apis/FoodFactsApi.js
@@ -1,13 +1,21 @@
-export default async function GetFoodInformation (data) {
-    console.log("Getting food information");
-        await fetch('https://world.openfoodfacts.org/api/v2/product/' + data + '?fields=product_name,nutriscore_data,ingredients', {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-            },  
-        })
-        .then((response) => response.json())
-        .then(response => {
-            console.log(response);
-        })
-};
+export default async function getProductInformation (data) {
+    return await fetch('https://world.openfoodfacts.org/api/v2/product/' + data + '?fields=brands,product_name,nutriments,ingredients', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        }  
+    })
+    .then((response) => response.json())
+    .then((response) => {
+        if(response.status_verbose == "product not found"){
+             return "Product not found";
+        } else {
+            return {
+                brand_name: response.product.brands,
+                product_name: response.product.product_name,
+                nutriments: response.product.nutriments,
+                ingredients: response.product.ingredients
+            };
+        }
+    })
+}        
--- screens/BarcodeScanner.js
+++ screens/BarcodeScanner.js
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import { Text, View, StyleSheet, Button, Image, TextInput, StatusBar, Alert, TouchableOpacity } from 'react-native';
 import { BarCodeScanner } from 'expo-barcode-scanner';
-import  getFoodInformation  from '../apis/FoodFactsApi.js';
+import  getProductInformation  from '../apis/FoodFactsApi.js';
+import { useNavigation } from '@react-navigation/native';
+
 
 const BarcodeScanner = () => {
     const [hasPermission, setHasPermission] = useState(null);
     const [scanned, setScanned] = useState(false);
     const [text, setText] = useState("Not yet scanned");
+    const [productList, updateProductList] = useState([]);
     /*
     useEffect(() => {
       askPermissions();
@@ -20,30 +23,57 @@ const BarcodeScanner = () => {
       })();
     };
 
-    const handleBarCodeScanned = ({ type, data }) => {
+    const addProductToList = (product) => {
+      updateProductList([...productList, product]);
+      setScanned(false);
+    };
+
+    const handleBarCodeScanned = async ({ type, data }) => {
       setScanned(true);
-      Alert.alert(
-        "Alert Title",
-        "My Alert Msg",
-        [
+      const productInformation = await getProductInformation(data)
+      console.log(productInformation)
+      if(productInformation == "Product not found") {
+        Alert.alert(
+          "Alert Title",
+          `The product scanned is not found in our database`,
+          [
             {
                 text: "Cancel",
-                onPress: () => console.log("Cancel Pressed"),
-                style: "cancel"
-            },
-            { 
-                text: "Scan again?", 
                 onPress: () => setScanned(false),
-                style: "ok"
-            },
-            { 
-                text: "See ingredients", 
-                onPress: () => getFoodInformation(data),
-                style: "ok"
+                style: "cancel"
             }
-        ]
+          ]
+      )} 
+      else if (!productInformation.nutriments || !productInformation.ingredients) {
+        Alert.alert(
+          "Alert Title",
+          `Unable to retrieve nutritional information for this product`,
+          [  
+            {
+              text: "Cancel",
+              onPress: () => setScanned(false),
+              style: "cancel"
+            }
+          ]
+      )}
+      else {
+        Alert.alert(
+          "Alert Title", 
+          `The product scanned is made by ${productInformation.brand_name}, the product is ${productInformation.product_name}`,
+          [
+              {
+                  text: "Cancel",
+                  onPress: () => setScanned(false),
+                  style: "cancel"
+              },
+              { 
+                  text: "This product is correct", 
+                  onPress: () => addProductToList(productInformation),
+                  style: "ok"
+              }
+          ]
         );
-      //alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+      }
     };
 
     if (hasPermission && hasPermission) {
@@ -52,8 +82,14 @@ const BarcodeScanner = () => {
           <View>
             <BarCodeScanner
               onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
-              style={{ height: '100%', width: '100%' }}
+              style={{ height: '90%', width: '100%' }}
             />
+            <TouchableOpacity
+              onPress={() => console.log(productList)}
+              style={styles.button}
+              >
+              <Text style={styles.buttonText}>View Scanned Item/Items</Text>
+            </TouchableOpacity>
           </View>
         );
       }
@@ -79,4 +115,15 @@ const BarcodeScanner = () => {
 
 export default BarcodeScanner;
 
-const styles = StyleSheet.create({}); 
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: "#2196F3",
+    padding: 20,
+    borderRadius: 5,
+    margin: 10,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: "#fff",
+  },
+}); 
