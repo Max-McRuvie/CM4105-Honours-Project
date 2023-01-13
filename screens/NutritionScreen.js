fb0126af62cfafa098d2c8d6c4a51b97dcad2465
max mcruvie <mcruvie@hotmail.co.uk>
2023-01-13 16:45:37 +0000

    Refactor nutrition screen

--- screens/NutritionScreen.js
+++ screens/NutritionScreen.js
@@ -6,45 +6,65 @@ import {ProductListContext} from '../components/ProductListContext';
 const NutritionScreen = () => {
     const { productList } = useContext(ProductListContext)
 
-    if(productList.length === 0){
-        return (
-            <>
-                <Text style={{fontSize: 30}}>No list present</Text>
-            </>
-        )
-    } else {
-        return (
-            <View style={styles.container}>
-                <View style={styles.topContainer}> 
-                    <Text style={{fontSize: 30}}>Nutrition Page</Text>
-                </View>
+    return (
+        <View style={styles.container}>
+            <View style={styles.topContainer}> 
+                <Text style={{fontSize: 30}}>Nutrition Page</Text>
+            </View>
+            {productList.length === 0 ? (
                 <View style={styles.middleContainer}>
-                <ScrollView>
-                    {productList.map((product, index) => {
-                        return (
+                    <Text style={{fontSize: 30}}>No list present</Text>
+                </View>
+            ) : (
+                <View style={styles.middleContainer}>
+                    <ScrollView>
+                        {productList.map((product, index) => (
                             <View key={index} style={styles.productContainer}>
-                                <Text style={styles.ingredientsTitle}></Text>
-                                <Text style={styles.product }>Brand Name: {product.brand_name}</Text>
-                                <Text style={styles.productTitle}>Product Name: {product.product_name}</Text>
+                                <Text style={styles.productTitle}>
+                                    Product Name: {product.product_name}
+                                </Text>
+                                <Text style={styles.product }>
+                                    Brand Name: {product.brand_name}
+                                </Text>
                                 <View style={styles.ingredientsContainer}>
-                                    <Text>Ingredients: {product.ingredients}</Text>
+                                    <Text>
+                                        Ingredients: {product.ingredients}
+                                    </Text>
                                 </View>
                                 <View style={styles.nutritionContainer}>
-                                    <Text>Energy: {product.nutriments.energy_value} {product.nutriments.energy_unit}</Text>
-                                    <Text>Fat: {product.nutriments.fat_value} {product.nutriments.fat_unit}</Text>
-                                    <Text>Saturated Fat: {product.nutriments.saturated_fat_value} {product.nutriments.saturated_fat_unit}</Text>
-                                    <Text>Carbohydrates: {product.nutriments.carbohydrates_value} {product.nutriments.carbohydrates_unit}</Text>
-                                    <Text>Sugars: {product.nutriments.sugars_value} {product.nutriments.sugars_unit}</Text>
-                                    <Text>Fiber: {product.nutriments.fiber_value} {product.nutriments.fiber_unit}</Text>
-                                    <Text>Proteins: {product.nutriments.proteins_value} {product.nutriments.proteins_unit}</Text>
-                                    <Text>Salt: {product.nutriments.salt_value} {product.nutriments.salt_unit}</Text>
-                                    <Text>Sodium: {product.nutriments.sodium_value} {product.nutriments.sodium_unit}</Text>
+                                    <Text>
+                                        Energy: {product.nutriments.energy_value} {product.nutriments.energy_unit}
+                                        </Text>
+                                    <Text>
+                                        Fat: {product.nutriments.fat_value} {product.nutriments.fat_unit}
+                                        </Text>
+                                    <Text>
+                                        Saturated Fat: {product.nutriments.saturated_fat_value} {product.nutriments.saturated_fat_unit}
+                                        </Text>
+                                    <Text>
+                                        Carbohydrates: {product.nutriments.carbohydrates_value} {product.nutriments.carbohydrates_unit}
+                                        </Text>
+                                    <Text>
+                                        Sugars: {product.nutriments.sugars_value} {product.nutriments.sugars_unit}
+                                        </Text>
+                                    <Text>
+                                        Fiber: {product.nutriments.fiber_value} {product.nutriments.fiber_unit}
+                                        </Text>
+                                    <Text>
+                                        Proteins: {product.nutriments.proteins_value} {product.nutriments.proteins_unit}
+                                        </Text>
+                                    <Text>
+                                        Salt: {product.nutriments.salt_value} {product.nutriments.salt_unit}
+                                        </Text>
+                                    <Text>
+                                        Sodium: {product.nutriments.sodium_value} {product.nutriments.sodium_unit}
+                                        </Text>
                                 </View>
                             </View>
-                        )
-                    })}
-                </ScrollView>
+                        ))}
+                    </ScrollView>
                 </View>
+            )}
                 <View style={styles.footerContainer}>
                     <NavigationBar />
                 </View>
@@ -52,21 +72,6 @@ const NutritionScreen = () => {
         )
     }
 
-    // return (
-    //     <View style={styles.container}>
-    //         <View style={styles.topContainer}> 
-    //             <Text style={{fontSize: 30}}>{productList[0][0].brand_name}</Text>
-    //         </View>
-        
-    //         <View style={styles.middleContainer}>
-                
-    //         </View>
-    //         <View style={styles.footerContainer}>
-    //             <NavigationBar />
-    //         </View>
-    //     </View>
-    // )
-}
 
 export default NutritionScreen
 
