2565c0b2c64871d3912a5bf5fcd2fffab09c6ace
max mcruvie <mcruvie@hotmail.co.uk>
2023-01-13 16:46:19 +0000

    Error handling for undefined ingredients

--- apis/FoodFactsApi.js
+++ apis/FoodFactsApi.js
@@ -1,21 +1,29 @@
-export default async function getProductInformation (data) {
-    return await fetch('https://world.openfoodfacts.org/api/v2/product/' + data + '?fields=brands,product_name,nutriments,ingredients', {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-        }  
-    })
+export default async function getProductInformation(data) {
+  return await fetch(
+    "https://world.openfoodfacts.org/api/v2/product/" +
+      data +
+      "?fields=brands,product_name,nutriments,ingredients",
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  )
     .then((response) => response.json())
     .then((response) => {
-        if(response.status_verbose == "product not found"){
-             return "Product not found";
-        } else {
-            return {
-                brand_name: response.product.brands,
-                product_name: response.product.product_name,
-                nutriments: response.product.nutriments,
-                ingredients: response.product.ingredients.map((ingredient) => (ingredient.text)),
-            };
-        }
-    })
-}        
+      console.log(response.product.ingredients);
+      if (response.status_verbose == "product not found") {
+        return "Product not found";
+      } else {
+        return {
+          brand_name: response.product.brands,
+          product_name: response.product.product_name,
+          nutriments: response.product.nutriments,
+          ingredients: response.product.ingredients
+            ? response.product.ingredients.map((ingredient) => ingredient.text)
+            : "No ingredients found",
+        };
+      }
+    });
+}
