a021872fcff99dc24063c2c0241d95a69685537d
max mcruvie <mcruvie@hotmail.co.uk>
2023-01-17 12:53:25 +0000

    Authentication Flow

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
+import { ProductListContextProvider } from './context/ProductListContext';
+import { auth } from './firebase/firebaseConfig'
 
 import HomeScreen from './screens/HomeScreen';
 import LoginScreen from './screens/LoginScreen';
@@ -10,37 +12,65 @@ import NutritionScreen from './screens/NutritionScreen';
 import RecipeScreen from './screens/RecipeScreen';
 import ProfileScreen from './screens/ProfileScreen';
 import BarcodeScanner from './screens/BarcodeScanner';
-import { ProductListContextProvider } from './components/ProductListContext';
+
 
 const Stack = createStackNavigator();
 
 //Defining an object with default options for all stack screens
 const stackScreenOptions = { headerShown: false };
 
+const createStackScreens = (screens, options) => screens.map(({ name, component }) => (
+  <Stack.Screen key={name} name={name} component={component} options={options} />
+));
+
 // Creating an array of stack screens and mapping through it to create the screens
-const StackScreens = [
+const signedInScreens = [
   { name: "Home", component: HomeScreen },
-  { name: "Login", component: LoginScreen },
-  { name: "Register", component: RegisterScreen },
   { name: "Nutrition", component: NutritionScreen },
   { name: "Recipes", component: RecipeScreen },
   { name: "Profile", component: ProfileScreen },
   { name: "BarcodeScanner", component: BarcodeScanner },
-].map(({ name, component }) => (
-  <Stack.Screen key={name} name={name} component={component} options={stackScreenOptions} />
-));
+]
+
+const signedOutScreens = [
+  { name: "Login", component: LoginScreen },
+  { name: "Register", component: RegisterScreen },
+]
+
 
 // Exported App function component
 export default function App() {
-  // Render the layout
-  return (
-    // Wrapping the layout with AppContext Provider to make the productListContext object available to all child components
+  // State to keep track of the user's authentication status
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    const subscriber = auth.onAuthStateChanged(onAuthStateChanged);
+    return subscriber; // unsubscribe on unmount
+  }, []);
+
+  const screens = useMemo(() => (user ? signedInScreens : signedOutScreens), [user]);
+
+  const onAuthStateChanged = (result) => {
+    setUser(result)
+    if (initializing) setInitializing(false);
+  } 
+
+  if(initializing) return null;
+
+
+  // FIXME: Users name is not displayed on initial load of home screen, but is displayed on subsequent loads
+  return user ? (
     <ProductListContextProvider>
       <NavigationContainer>
-        <Stack.Navigator>{StackScreens}</Stack.Navigator>
+        <Stack.Navigator>{createStackScreens(screens, stackScreenOptions)}</Stack.Navigator>
       </NavigationContainer>
     </ProductListContextProvider>
-  );
+  ) : (
+    <NavigationContainer>
+      <Stack.Navigator>{createStackScreens(screens, stackScreenOptions)}</Stack.Navigator>
+    </NavigationContainer>
+  )
 }
 
 const styles = StyleSheet.create({
