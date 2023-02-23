6469c8b3b84cbed090655eb4683d244116fc15c5
max mcruvie <mcruvie@hotmail.co.uk>
2023-02-23 15:10:00 +0000

    display ingredients and instructions

--- screens/RecipeScreen.js
+++ screens/RecipeScreen.js
@@ -1,42 +1,53 @@
-import { StyleSheet, Text, View, ScrollView, Button, Image } from 'react-native'
+import { RecipeScreenStylesheet, Text, View, ScrollView, Button, Image } from 'react-native'
 import React, {useContext} from 'react'
 import NavigationBar from '../components/NavigationBar'
 import {AppContext} from '../context/AppContext';
 import { useNavigation } from '@react-navigation/native';
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import { RecipeScreenStyles } from '../styles/stylesheet';
 
 const RecipeIndivisualScreen = (prop) => {
     const navigation = useNavigation();
-    const recipe = prop.route.params
+    const recipe = prop.route.params.recipe
+    const instructions = prop.route.params.instructions;
 
-    console.log(recipe)
+    console.log('jeff')
+    // instructions.map((instruction) => console.log(instruction.steps))
+    //console.log(instructions[0].steps.map((instruction) => console.log(instruction)))
+    console.log(instructions)
 
     return (
-      <View style={styles.container}>
-        <View style={styles.topContainer}>
-        <Image source={{uri: recipe.image}} style={styles.recipeImage} resizeMode='contain'/> 
+      <View style={RecipeScreenStyles.container}>
+        <View style={RecipeScreenStyles.topContainer}>
+        <Image source={{uri: recipe.image}} style={RecipeScreenStyles.recipeImage} resizeMode='contain'/> 
         </View>
-        <View style={styles.middleContainer}>
+        <View style={RecipeScreenStyles.middleContainer}>
             <Text style={{fontSize: 30 }}>{recipe.title}</Text>
             <Text style={{fontSize: 20, paddingTop: '5%'}}>Ingredients</Text>
             {recipe.missedIngredients.map((ingredient, index) => (
-                    <View key={index} style={styles.ingredientContainer}>
-                        <Text style={styles.ingredientName}>{ingredient.name}</Text> 
-                        <Text style={styles.ingredientName}>{ingredient.amount} {ingredient.unit}</Text>
+                    <View key={index} style={RecipeScreenStyles.ingredientContainer}>
+                        <Text style={RecipeScreenStyles.ingredientName}>{ingredient.name}</Text> 
+                        <Text style={RecipeScreenStyles.ingredientName}>{ingredient.amount} {ingredient.unit}</Text>
                     </View>
             ))}
             {recipe.usedIngredients.map((ingredient, index) => (
-                    <View key={index} style={styles.ingredientContainer}>
-                        <Text style={styles.ingredientName}>{ingredient.name}</Text> 
-                        <Text style={styles.ingredientName}>{ingredient.amount} {ingredient.unit}</Text>
+                    <View key={index} style={RecipeScreenStyles.ingredientContainer}>
+                        <Text style={RecipeScreenStyles.ingredientName}>{ingredient.name}</Text> 
+                        <Text style={RecipeScreenStyles.ingredientName}>{ingredient.amount} {ingredient.unit}</Text>
                     </View>
             ))}
             <Text style={{fontSize: 20, paddingTop: '5%'}}>Instructions</Text>
+            {instructions[0].steps.map((instruction, index) => (
+                <View key={index} style={RecipeScreenStyles.ingredientContainer}>
+                    <Text style={RecipeScreenStyles.ingredientName}>{index + 1}</Text>
+                    <Text style={RecipeScreenStyles.ingredientName}>{instruction.step}</Text>
+                </View>
+            )
+            )}
             
 
         </View>
 
-        <View style={styles.footerContainer}>
+        <View style={RecipeScreenStyles.footerContainer}>
                     <NavigationBar />
                 </View>
       </View>
@@ -45,39 +56,3 @@ const RecipeIndivisualScreen = (prop) => {
 }
 
 export default RecipeIndivisualScreen
-
-const styles = StyleSheet.create({
-    container: {
-        height: '100%',
-        width: '100%',
-    },
-    topContainer: {
-        width: '100%',
-        height: '37%',
-        justifyContent: 'center',
-    },
-    middleContainer: {
-        width: '100%',
-        height: '68%',
-        paddingLeft: '5%',
-    },
-    footContainer: {
-        height: '30%',
-    },
-    recipeImage: {
-        width: '100%',
-        height: '100%',
-    },
-    ingredientContainer: {
-        margin: '1%',
-        marginRight: '5%',
-        marginLeft: '5%',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-    },
-    ingredientName: {
-        fontSize: 15,
-          
-    }
-
-})
