6e44461b8a66e799c552648d093ea5b2ebf7c3f9
max mcruvie <mcruvie@hotmail.co.uk>
2023-02-23 22:11:03 +0000

    New text styles applied

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,7 +2,7 @@ import { StyleSheet, Text, View, Button } from 'react-native'
 import React, {useState, useEffect, useContext} from 'react'
 import { auth } from '../firebase/firebaseConfig'
 import NavigationBar from '../components/NavigationBar'
-import { containerStyles } from '../styles/stylesheet'
+import { containerStyles, TextStyles } from '../styles/stylesheet'
 
 const HomeScreen = () => {
     
@@ -20,11 +20,11 @@ const HomeScreen = () => {
     return (
         <View style={containerStyles.container}>
             <View style={containerStyles.topContainer}> 
-                    <Text style={{fontSize: 30}}>Welcome {name || ''}</Text>
+                    <Text style={TextStyles.title}>Welcome {name || ''}</Text>
             </View>
             
             <View style={containerStyles.middleContainer}>
-                <Text style={{fontSize: 15}}>
+                <Text style={TextStyles.text}>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sit amet iaculis augue, vitae tempus ligula. Curabitur luctus imperdiet est, vitae accumsan elit posuere in. Phasellus quis malesuada odio. Curabitur cursus ut massa ac auctor. Nunc vitae sapien non lacus molestie egestas. Curabitur consequat, nibh et eleifend suscipit, nisi nisi condimentum eros, non congue nulla quam non ipsum. Phasellus ornare nisi at dui porttitor, non ultricies turpis volutpat. Vestibulum et consequat odio.
                 </Text>
             </View>
