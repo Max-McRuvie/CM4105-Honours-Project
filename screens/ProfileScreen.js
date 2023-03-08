598df134e3e097b81e29a0b4ee95430408a75715
max mcruvie <mcruvie@hotmail.co.uk>
2023-03-08 07:13:17 +0000

    Moved stylesheet to global stylesheet

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -2,18 +2,19 @@ import { StyleSheet, Text, View, Button } from 'react-native'
 import React from 'react'
 import NavigationBar from '../components/NavigationBar'
 import { auth } from '../firebase/firebaseConfig'
+import { containerStyles, TextStyles, buttonStyles } from '../styles/stylesheet'
 
 const ProfileScreen = () => {
   return (
-    <View style={styles.container}>
-        <View style={styles.topContainer}> 
-                <Text style={{fontSize: 30}}>Profile</Text>
+    <View style={containerStyles.container}>
+        <View style={containerStyles.topContainer}> 
+                <Text style={TextStyles.title}>Profile</Text>
         </View>
         
-        <View style={styles.middleContainer}>
-            <Button title="Logout" style={styles.button} onPress={() => auth.signOut()} />
+        <View style={containerStyles.middleContainer}>
+            <Button title="Logout" style={buttonStyles.button} onPress={() => auth.signOut()} />
         </View>
-        <View style={styles.footerContainer}>
+        <View style={containerStyles.footerContainer}>
             <NavigationBar />
         </View>
     </View>
@@ -21,24 +22,3 @@ const ProfileScreen = () => {
 }
 
 export default ProfileScreen
-
-const styles = StyleSheet.create({
-    container: {
-        height: '100%',
-        width: '100%',
-    },
-    topContainer: {
-        width: '80%',
-        height: '30%',
-        justifyContent: 'center',
-        marginLeft: "10%",
-    },
-    middleContainer: {
-        width: '80%',
-        height: '60%',
-        alignItems: 'center',
-    },
-    footContainer: {
-        height: '30%',
-    },
-})
