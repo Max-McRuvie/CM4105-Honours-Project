cf5d0d279fa1e9f1db3aabe3005fe9fd12800dd6
max mcruvie <mcruvie@hotmail.co.uk>
2023-03-20 18:06:32 +0000

    Can add and remove bookmarked recipes

--- screens/RecipeScreen.js
+++ screens/RecipeScreen.js
@@ -1,8 +1,23 @@
 import { Text, View, ScrollView, Image, Pressable } from 'react-native'
 import React, {useState} from 'react'
+import { Ionicons } from '@expo/vector-icons';
+import {addFavoriteRecipe, removeFavoriteRecipe} from '../firebase/firebaseConfig'
 
 import { RecipeScreenStyles, TextStyles, containerStyles } from '../styles/stylesheet';
-const RecipeIndivisualScreen = ({ route: { params: { recipe, instructions } } }) => {
+const RecipeIndivisualScreen = ({ route: { params: { recipe, isFav } } }) => {
+
+    const [isFavourite, updateIsFavourite] = useState(isFav)
+
+    const changeFavourite = async () => {
+        // await addFavoriteRecipe(recipe)
+        if(!isFavourite) {
+            updateIsFavourite(true)
+            addFavoriteRecipe(recipe.id)
+        } else {
+            updateIsFavourite(false)
+            removeFavoriteRecipe(recipe.id)
+        }
+    }
 
     return (
       <View style={containerStyles.container}>
@@ -13,10 +28,35 @@ const RecipeIndivisualScreen = ({ route: { params: { recipe, instructions } } })
             <ScrollView>
                     <View style={{flexDirection: 'row'}}>
                         <Text style={[TextStyles.recipeTitle, {width: '80%', marginBottom: '2%'}]}>{recipe.title}</Text>
-                        
+                        <Pressable style={RecipeScreenStyles.button}>
+                            <Ionicons
+                                style={{padding: '2%'}}
+                                name= {isFavourite ? "heart" : "heart-outline"}
+                                color="red"
+                                onPress={() => { changeFavourite(), updateIsFavourite(!isFavourite)}}
+                                size={30}
+                            />
+                        </Pressable>
+
                     </View>
+                    <Text style={[TextStyles.subHeader, {width: '80%', marginBottom: '2%'}]}>Ready in {recipe.readyInMinutes} minutes</Text>
+                    <Text style={[TextStyles.subHeader, {width: '80%', marginBottom: '2%'}]}>Serves: {recipe.servings}</Text>
+                    <View style={RecipeScreenStyles.bubbleContainer}>
+                        <Text style={TextStyles.header}>Summary</Text>
+                            <View style={RecipeScreenStyles.contentContainer}>
+                                <Text style={TextStyles.text}>{recipe.summary.replace(/<.*?>/g, '')}</Text>
+                            </View>
+                    </View>
+
                     <View style={RecipeScreenStyles.bubbleContainer}>
                         <Text style={TextStyles.header}>Ingredients</Text>
+                        {recipe.extendedIngredients.map((ingredient, index) => (
+                            <View key={index} style={[RecipeScreenStyles.contentContainer, {flexDirection: 'row'}]}>
+                                <Text style={TextStyles.text}>- {ingredient.original}</Text>
+                            </View>
+                        ))}
+
+                        {/* <Text style={TextStyles.header}>Ingredients</Text>
                         {recipe.missedIngredients.map((ingredient, index) => (
                                 <View key={index} style={RecipeScreenStyles.contentContainer}>
                                     <Text style={TextStyles.text}>{ingredient.name}</Text> 
@@ -28,17 +68,19 @@ const RecipeIndivisualScreen = ({ route: { params: { recipe, instructions } } })
                                     <Text style={TextStyles.text}>{ingredient.name}</Text> 
                                     <Text style={TextStyles.text}>{ingredient.amount} {ingredient.unit}</Text>
                                 </View>
-                        ))}
+                        ))} */}
                     </View>
                     <View style={RecipeScreenStyles.bubbleContainer}>
                         <Text style={TextStyles.header}>Instructions</Text>
-                        {instructions[0].steps.map((instruction, index) => (
-                            <View key={index} style={RecipeScreenStyles.contentContainer}>
-                                <Text style={TextStyles.text}>{index + 1} - </Text>
-                                <Text style={TextStyles.text}>{instruction.step}</Text>
+                        {recipe.analyzedInstructions[0].steps.map((instruction, index) => (
+                            
+                            <View key={index} style={[RecipeScreenStyles.contentContainer, {flexDirection: 'column'}]}>
+                                <Text style={[TextStyles.subHeader, {borderBottomWidth: 1, width: "18%", marginBottom: '2%'}]}>{`Step ${index + 1}:`}</Text>
+                                <Text style={TextStyles.text}>{`${instruction.step}`}</Text>
                             </View>
-                        )
-                        )}
+
+                         ))}
+
                     </View>
                     </ScrollView>
             </View>
