2b5ee9b6b0e898b4155bef0c32afb39e410e531f
max mcruvie <mcruvie@hotmail.co.uk>
2023-03-20 12:30:52 +0000

    Using different api call

--- screens/RecipeListScreen.js
+++ screens/RecipeListScreen.js
@@ -3,49 +3,55 @@ import React, {useContext} from 'react'
 import {AppContext} from '../context/AppContext';
 import { RecipeListScreenStyles, containerStyles, TextStyles } from '../styles/stylesheet';
 
-import { getRecipeInstructions } from '../apis/SpoonacularApi.js'
+import { getRecipeInformation } from '../apis/SpoonacularApi.js'
 
 const RecipeScreen = ({navigation}) => {
     const context = useContext(AppContext)
 
     // Assign the recipe list context to a variable for easier access
     const recipeContext = context.recipeListState
+    // console.log(context.recipeListState.recipeList)
+    
 
-    const getInstructions = async (navigation, recipe) => {
-        let instructions = await getRecipeInstructions(recipe.id)
+    const getInformation = async (recipe) => {
+        let recipeInformation = await getRecipeInformation(recipe.id)
         // console.log(recipeResponse)
         // update the product list context with the new product
+        
         navigation.navigate('RecipeScreen', {
-            recipe: recipe,
-            instructions: instructions
+            recipe: recipeInformation,
         })
     }
+
+    let error;
+    if (recipeContext.recipeList.length === 0) {
+        error = 'No items have been searched'
+    } else if (recipeContext.recipeList.results.length < 1){
+        error ='No recipes found'
+    }
+    
+    
+
 
     return (
       <View style={containerStyles.container}>
         <View style={containerStyles.topContainer}>
           <Text style={TextStyles.title}>Recipes Page</Text>
         </View>
-        {recipeContext.recipeList.length === 0 ? (
+        {error ? (
             <View style={containerStyles.listContainer}>
-                <Text style={TextStyles.header}>No items have been searched</Text>
+                <Text style={TextStyles.header}>{error}</Text>
             </View>
         ) : (
             <View style={containerStyles.listContainer}>
                 <ScrollView>
-                    {recipeContext.recipeList.map((recipe, index) => (
+                    {recipeContext.recipeList.results.map((recipe, index) => (
                         <View key={index} style={RecipeListScreenStyles.recipeListContainer}>
-                            <Pressable onPress={() => {getInstructions(navigation, recipe)}} style={RecipeListScreenStyles.recipeContainer}>
+                            <Pressable onPress={() => {getInformation(recipe)}} style={RecipeListScreenStyles.recipeContainer}>
                                 <Image source={{uri: recipe.image}} style={RecipeListScreenStyles.image} resizeMode='contain' />
                                 <View style={RecipeListScreenStyles.textContainer}>
                                     <Text style={TextStyles.subHeader}>
                                         {recipe.title}
-                                    </Text>
-                                    <Text style={TextStyles.listText}>
-                                        usedIngredientCount: {recipe.usedIngredientCount}
-                                    </Text>
-                                    <Text style={TextStyles.listText}>
-                                        missedIngredientCount: {recipe.missedIngredientCount}
                                     </Text>
                                 </View>
                             </Pressable>
